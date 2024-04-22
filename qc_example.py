5f2b1bf6ae435cc041c2357ed2c2280507c26010
Eugene Pentland <epentland@2yfv.com>
2024-04-22 01:26:47 +0000

    added docstrings to qc class

--- qc_example.py
+++ qc_example.py
@@ -1,5 +1,5 @@
 # QC Example.py
-from devices.QC import QC
+from devices.QC import QC, QCBank
 from drivers.interface import MockDriver
 
 driver = MockDriver("device_id", "configs/QC_FTDI.json")
@@ -8,4 +8,4 @@ qc = QC(driver)
 qc.set_attenuation_db(15.5)
 qc.read_attenuation_db()
 qc.set_bypass_enable(0)
-qc.set_filter_bank(1)
+qc.set_filter_bank(QCBank.LPF_1GHz)
