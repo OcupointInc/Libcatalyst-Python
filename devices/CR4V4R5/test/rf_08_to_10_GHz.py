374741d76713a95765a84d04609672e514f406bf
Eugene Pentland <epentland@2yfv.com>
2025-02-19 15:01:00 +0000

    test with no install

--- devices/CR4V4R5/test/rf_08_to_10_GHz.py
+++ devices/CR4V4R5/test/rf_08_to_10_GHz.py
@@ -1,14 +1,8 @@
-import sys
-import os
-
-# Add the project root to sys.path
-sys.path.append(os.path.dirname(os.path.abspath("/home/ocupoint/Libcatalyst-Python/libcatalyst/devices/CR4V5.py")))
-
 from libcatalyst.drivers.ftdi_driver import FTDISPIDriver
-from libcatalyst.devices.CR4V5 import CR4V4R5
+from CR4V5 import CR4V4R5
 
 # Initialize driver and device
-driver = FTDISPIDriver("configs/CR4_V4_FTDI.json", debug=True)
+driver = FTDISPIDriver("../configs/CR4_V4_FTDI.json", debug=True)
 cr4 = CR4V4R5(driver, clock_frequency_mhz=100)
 
 # Set the attenuation and RF filter settings
