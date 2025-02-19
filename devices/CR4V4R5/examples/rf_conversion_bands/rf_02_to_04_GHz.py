bfa16a942691ecd9dfb8a6756a880ef71c8e026c
Eugene Pentland <eugenepentland@gmail.com>
2025-02-19 14:18:22 -0500

    fixed the cr4 tune filter examples

--- devices/CR4V4R5/examples/rf_conversion_bands/rf_02_to_04_GHz.py
+++ devices/CR4V4R5/examples/rf_conversion_bands/rf_02_to_04_GHz.py
@@ -1,12 +1,13 @@
 from libcatalyst.drivers.ftdi_driver import FTDISPIDriver
 from libcatalyst.devices.CR4V5 import CR4V4R5
+
 # Initialize driver and device
 driver = FTDISPIDriver("configs/CR4_V4_FTDI.json", debug=True)
 cr4 = CR4V4R5(driver, clock_frequency_mhz=100)
 
 # Set the attenuation and RF filter settings
 cr4.set_attenuation_db(channels=[1,2,3,4], attenuation_db=0)
-cr4.tune_filters(lpf_switch=0, lpf_band=0, hpf_switch=0, hpf_band=0)
+cr4.tune_filters(channels=[1,2,3,4], lpf_switch=0, lpf_band=0, hpf_switch=0, hpf_band=0)
 
 # Set the switch state, single or dual convert
 cr4.set_switch("AB", "dual")
